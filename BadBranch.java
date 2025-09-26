077385f86957ee4a2024e6e099a8908d7ef2fb6f
mbuescher <mbuescher@hb.edu>
2025-09-25 20:34:00 -0400

    Create BadBranch.java

---
+++ BadBranch.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BadBranch
+{
+
+  /** Converts a numeric grade (percent) to a letter grade */
+  public static String getLetterGrade(int grade)
+  {
+    String ans;
+    if (grade >= 90)  {
+      ans = "A";
+    }
+    if (grade >= 80)  {
+      ans = "B"; 
+    }
+    if (grade >= 70)  {
+      ans = "C";
+    }
+    if (grade >= 60)  {
+      ans = "D";
+    }
+    else {
+      ans = "F";
+    }
+    return ans;
+  }
+
+  /** a main method for testing   */
+  public static void main (String[] args)
+  {
+    Scanner kbd = new Scanner (Systen.in);
+    System.out.println("Enter a numeric grade: ");
+    int grade = kbd.nextInt();
+    while (grade >= 0) {
+      System.out.println ("A grade of " + grade + " is a(n) " + getLetterGrade(grade));
+      grade = kbd.nextInt();
+    }
+    kbd.close();
+    
